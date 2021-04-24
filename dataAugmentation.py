9ebeba23d4afc8bdce35e394b5e97eaa1c6a66a9
HELIOPOTELICKI <helio.p2000@hotmail.com>
2021-04-23 23:32:01 -0300

    refactored

--- dataAugmentation.py
+++ dataAugmentation.py
@@ -1,9 +1,8 @@
-from keras.preprocessing.image import load_img, save_img, img_to_array, array_to_img, ImageDataGenerator
+from keras.preprocessing.image import load_img, img_to_array, ImageDataGenerator
 import matplotlib.pyplot as plt
 from numpy import expand_dims
 from os import listdir
 import timeit
-import time
 import cv2
 
 # Path da pasta de entrada
@@ -13,18 +12,22 @@ dirs = listdir(path)
 
 start = timeit.default_timer()
 for i in range(0, len(dirs)):
-    #Carrega imagem para cv2
+    # prefixo para salvamento
+    prefix = str(dirs[i])[:-4]
+
+    # Carrega imagem para cv2
     imgCV = cv2.imread(f'entry\{dirs[i]}')
-    # Carrega para Keras
+
+    # Carrega imagem para Keras
     imgKR = load_img(f'entry\{dirs[i]}')
-
-    # Flip
-    img_flipped = cv2.flip(imgCV, 1)
-    cv2.imwrite(f'out\{str(dirs[i])[:-4]}_reverse.jpg', img_flipped)
-
-    # Brilho
     data = img_to_array(imgKR)
     samples = expand_dims(data, 0)
+
+    # Inverte
+    img_flipped = cv2.flip(imgCV, 1)
+    cv2.imwrite(f'out\{prefix}_reverse.jpg', img_flipped)
+
+    # Brilho
     datagen = ImageDataGenerator(brightness_range=[0.2, 1.0])
     it = datagen.flow(samples, batch_size=1)
 
@@ -33,15 +36,13 @@ for i in range(0, len(dirs)):
         imgB = batch[0].astype('uint8')
         imgEnd = plt.imshow(imgB)
         plt.axis('off')
-        plt.savefig(f'out\{str(dirs[i])[:-4]}_brilho_{j}.jpg',
+        plt.savefig(f'out\{prefix}_brilho_{j}.jpg',
                     bbox_inches='tight',
                     pad_inches=0,
                     format='jpg',
                     dpi=140)
 
-    #Zoom
-    data = img_to_array(imgKR)
-    samples = expand_dims(data, 0)
+    # Zoom
     datagen = ImageDataGenerator(zoom_range=[0.5, 1.0])
     it = datagen.flow(samples, batch_size=1)
 
@@ -50,15 +51,13 @@ for i in range(0, len(dirs)):
         imgB = batch[0].astype('uint8')
         imgEnd = plt.imshow(imgB)
         plt.axis('off')
-        plt.savefig(f'out\{str(dirs[i])[:-4]}_zoom_{k}.jpg',
+        plt.savefig(f'out\{prefix}_zoom_{k}.jpg',
                     bbox_inches='tight',
                     pad_inches=0,
                     format='jpg',
                     dpi=140)
 
     # Deslocamento
-    data = img_to_array(imgKR)
-    samples = expand_dims(data, 0)
     datagen = ImageDataGenerator(width_shift_range=[-200, 200])
     it = datagen.flow(samples, batch_size=1)
 
@@ -67,11 +66,11 @@ for i in range(0, len(dirs)):
         imgB = batch[0].astype('uint8')
         imgEnd = plt.imshow(imgB)
         plt.axis('off')
-        plt.savefig(f'out\{str(dirs[i])[:-4]}_dislocada_{l}.jpg',
+        plt.savefig(f'out\{prefix}_dislocada_{l}.jpg',
                     bbox_inches='tight',
                     pad_inches=0,
                     format='jpg',
                     dpi=140)
 
 end = timeit.default_timer()
-print('tempo de processamento: %f segundos' % (end - start))
+print('Tempo de processamento: %f segundos' % (end - start))
